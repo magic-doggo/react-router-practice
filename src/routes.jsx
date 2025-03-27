3b15a7c1ee471c17263da20fbc07aa882c3a27d2
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2025-03-27 07:32:18 +0000

    Default exercise files React Router

---
+++ src/routes.jsx
@@ -0,0 +1,19 @@
+import App from "./App";
+import Profile from "./Profile";
+import ErrorPage from "./ErrorPage";
+import { element } from "prop-types";
+import DefaultProfile from "./DefaultProfile";
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },//diff between elem and error?
+  {
+    path: "profile/:name",
+    element: <Profile />,
+  },
+];
+
+export default routes;
